219139f674eff23a54703e8a5267e8b13030cbda
maisaFit <129098319+maisaFit@users.noreply.github.com>
2023-03-28 17:48:30 -0400

    Add files via upload

---
+++ Course.js
@@ -0,0 +1,19 @@
+class Course{
+    constructor(
+      title,
+      number,
+      location,
+      instructor,
+      academic_period,
+    
+    ){
+      this.title= title,
+      this.number= number,
+      this.location = location,
+      this.instructor = instructor,
+      this.academic_period = academic_period
+    };
+  
+  }
+
+  export default Course;
